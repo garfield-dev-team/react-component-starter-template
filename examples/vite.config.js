b9ba1e2ad8c57d3650918d29cff5d08a04b3856c
Jiacheng Dong <2334173653@qq.com>
2022-07-23 12:57:08 +0800

    feat: vite config

--- examples/vite.config.js
+++ examples/vite.config.js
@@ -1,9 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "node:path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.tsx'),
+      // UMD 格式的包名
+      name: 'MyLib',
+      fileName: (format) => `my-lib.${format}.js`
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['antd', 'react', 'react-dom'],
+    }
+  }
+});
 
 // TODO: implement lib mode
