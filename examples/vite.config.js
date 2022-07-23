f0aa8be0dcc32459316d79516fe65dddcd947b3f
Jiacheng Dong <2334173653@qq.com>
2022-07-23 21:47:26 +0800

    feat: vite output format

--- examples/vite.config.js
+++ examples/vite.config.js
@@ -20,6 +20,7 @@ export default defineConfig({
   },
   build: {
     lib: {
+      formats: ["cjs", "es"],
       entry: path.resolve(__dirname, "src/index.tsx"),
       // UMD 格式的包名
       name: "MyLib",
@@ -27,9 +28,14 @@ export default defineConfig({
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: [/antd/, "react", "react-dom"]
+      external: [/antd/, "react", "react-dom"],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        // 如果不需要输出 UMD 格式则可以忽略
+        // globals: {
+        //   vue: "Vue"
+        // }
+      }
     }
   }
 });
-
-// TODO: implement lib mode
