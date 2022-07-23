56222481a85e2a6d4ceb3795e87958db59ee93ec
Jiacheng Dong <2334173653@qq.com>
2022-07-23 17:09:20 +0800

    feat: vite dev config

--- examples/src/index.tsx
+++ examples/src/index.tsx
@@ -1,12 +1,1 @@
-import * as React from "react";
-import { Button } from "antd";
-
-const MyButton: React.FC = () => {
-  return (
-    <div>
-      <Button>2333</Button>
-    </div>
-  );
-};
-
-export { MyButton };
+export { MyButton } from "@garfield-react-component/button";
--- examples/vite.config.js
+++ examples/vite.config.js
@@ -1,20 +1,33 @@
 import path from "node:path";
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import importPlugin from "vite-plugin-importer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: "classic"
+    }),
+    importPlugin({
+      libraryName: "antd",
+      libraryDirectory: "es",
+      style: "css"
+    })
+  ],
+  server: {
+    open: true
+  },
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.tsx'),
+      entry: path.resolve(__dirname, "src/index.tsx"),
       // UMD 格式的包名
-      name: 'MyLib',
+      name: "MyLib",
       fileName: (format) => `my-lib.${format}.js`
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['antd', 'react', 'react-dom'],
+      external: [/antd/, "react", "react-dom"]
     }
   }
 });
